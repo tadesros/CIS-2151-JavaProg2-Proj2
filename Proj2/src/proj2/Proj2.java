c9b19c1d04a1467e464234a240f543ec3809c965
Tom <tomadesrosiers@gmail.com>
2024-06-24 22:45:48 -0400

    gh

---
+++ Proj2/src/proj2/Proj2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proj2;
+
+/**
+ *
+ * @author tomad
+ */
+public class Proj2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
